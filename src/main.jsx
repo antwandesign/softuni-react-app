831956f4df0facb57497d40dd598f1fa483e7b67
antwandesign <39638303+antwandesign@users.noreply.github.com>
2022-08-04 18:21:01 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {RecoilRoot} from 'recoil'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+    <RecoilRoot>
+    <Router>
     <App />
-  </React.StrictMode>
+    </Router>
+    </RecoilRoot>
+
 )
